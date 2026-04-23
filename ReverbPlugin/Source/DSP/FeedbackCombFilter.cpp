3a715e16598162fe040215193295d9a1205ddbed
arendilnkplummer <arendilnkplummer@gmail.com>
2026-04-23 13:36:07 -0500

    added FeedbackCombFilter class

---
+++ ReverbPlugin/Source/DSP/FeedbackCombFilter.cpp
@@ -0,0 +1,34 @@
+/*
+  ==============================================================================
+
+    FeedbackCombFilter.cpp
+    Created: 21 Apr 2026 1:21:04pm
+    Author:  Student User
+
+  ==============================================================================
+*/
+
+#include "FeedbackCombFilter.h"
+
+
+float FeedbackCombFilter::processSample(float x, int channel){
+   fractionalDelay.processSample(x,chaneel);
+    
+}
+
+void FeedbackCombFilter::setFs(float sampleRate){
+    //updating delay value to new sampleRate
+    delay *= sampleRate/Fs;
+    
+    fractionalDelay.setFs(sampleRate);
+    Fs = sampleRate;
+    
+}
+
+void FeedbackCombFilter::setGain(float g){
+    gain = g;
+}
+
+void FeedbackCombFilter::setDelayMs(float ms){
+    delay = (ms/1000) * Fs;
+}
